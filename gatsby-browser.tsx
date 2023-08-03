0d6467401dd2de50c1d6f81127702fec8ebcf080
Johann Boumans <34610093+JohannBoumans@users.noreply.github.com>
2023-08-03 11:16:41 +0200

    feat: loading behavior

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,3 +1,4 @@
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import { WrapPageElement } from './src/components/wrap-page-element/wrapPageElement';
 
 export const wrapPageElement = WrapPageElement;
